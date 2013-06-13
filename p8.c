147362b90434f74898e7f96570fff73b0689447d
nham <hamann.nick@gmail.com>
2013-06-13 22:46:57 +0000

    p8 actually runs now

--- p8.c
+++ p8.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 
+void popseq(char *fname, char *arr)
+{
+    FILE *fp;
+    char buf[100];
+    int i, j = 0;
+
+    fp = fopen(fname, "r");
+
+    // WHY do i have to do 52 and not 51??
+    while(fgets(buf, 52, fp) != NULL) {
+        for(i = 0; i < 50; i++)
+            arr[j+i] = buf[i] - '0';
+        j += 50;
+    }
+
+}
+
 int main(void)
 {
-    int num[1000], cnt = 0, off=0, max=0, tmp;
+    char num[1000];
+    int cnt = 0, off=0, max=0, tmp;
+    popseq("p8.txt", &num[0]);
 
     while(cnt <= 995) {
         tmp = 1;
         for(off = 0; off < 5; off++)
             if(num[cnt+off] != 0)
-                tmp *= num[cnt+off]
+                tmp *= num[cnt+off];
             else
                 break;
         // terminated early due to zero, so just advanced cnt past it
         if(off < 5)
             cnt += off+1;
-        else if(tmp > max)
+        else if(tmp > max) {
             max = tmp;
+            printf(" > max is now %d, @ cnt = %d\n", max, cnt);
+            printf("cnt vals: %d, %d, %d, %d, %d\n", num[cnt], num[cnt+1], num[cnt+2], num[cnt+3], num[cnt+4]);
+        }
 
+        cnt += 1;
 
     }
-
-    
 
 }
