d5c9fbf7dbb230d8e613abc26f7ddab2e5309097
nham <hamann.nick@gmail.com>
2013-06-21 20:37:11 +0000

    finish p11 solution in go

--- go/p11.go
+++ go/p11.go
@@ -29,14 +29,18 @@ func maxseqinc(a []uint, length, width, inc int) int {
     max := 0
     var tmp int
 
-    for i := 0; i <= length - 1 - inc*(width-1); i += inc {
+    if length < width {
+        return 0
+    }
+
+    for i := 0; i < length - width; i++ {
         tmp = 1
-        for off := 0; off < width; off += inc {
-            tmp *= int(a[i+off])
+        for off := 0; off < width; off++ {
+            tmp *= int(a[(i+off)*inc])
         }
         if tmp > max {
             max = tmp
-            //fmt.Println(a[i:i+width])
+            //fmt.Println(a[i*inc:(i+width)*inc])
             //fmt.Println(" max -- ", max)
         }
     }
@@ -50,11 +54,11 @@ func main() {
     max := 0.0
 
     for i := 0; i < 20; i++ {
-        max = math.Max(max, float64(maxseqinc(a[i:], 20, 4, 1)))
-        max = math.Max(max, float64(maxseqinc(a[i*20:], 20, 4, 20)))
+        max = math.Max(max, float64(maxseqinc(a[i:], 20, 4, 20)))
+        max = math.Max(max, float64(maxseqinc(a[i*20:], 20, 4, 1)))
+
         max = math.Max(max, float64(maxseqinc(a[i:], 20-i, 4, 21)))
         max = math.Max(max, float64(maxseqinc(a[i:], i+1, 4, 19)))
-
     }
     fmt.Println(int(max))
 }
