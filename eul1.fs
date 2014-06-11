8910f451839a910d91f75395ba4081f6cc0047aa
nham <hamann.nick@gmail.com>
2014-06-11 15:54:46 -0400

    rename add2 to addtoNOS

--- eul1.fs
+++ eul1.fs
@@ -1,12 +1,12 @@
 : isdiv3 3 mod 0 = ;
 : isdiv5 5 mod 0 = ;
-: add2 swap over + swap ;
+: addtoNOS swap over + swap ;
 
 : eul1 0 999 BEGIN 
-    dup isdiv3 if add2 
-               else dup isdiv5 if add2 endif 
+    dup isdiv3 if addtoNOS 
+               else dup isdiv5 if addtoNOS endif 
                endif 
     1 - dup 0= 
 UNTIL ;
 
-( the above seems very ugly. what i'd like to say is "if its divisible by 3 or 5, add2, otherwise move on" )
+( the above seems very ugly. what i'd like to say is "if its divisible by 3 or 5, addtoNOS otherwise move on" )
